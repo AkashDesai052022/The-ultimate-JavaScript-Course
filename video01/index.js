ca5de6601af09a8154b42a2cc830029582f942b1
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-28 13:17:19 +0530

    Variables of JavaScript

---
+++ video01/index.js
@@ -0,0 +1,22 @@
+console.log("Variables in JavaScript");
+
+console.log("this is video no. 2");
+// that's why JS is dynamically typed language
+// JS is allowed to change variable type in runtime
+let a = 67;
+console.log(a);
+a = "harry"  // here we can redeclare variable and also reassign the value to it
+console.log(a);
+
+var $a = 7;
+var _a = 7;
+
+
+// var 8a = 7; // this is not allowed 
+// var var = 7;  // this is also not allowed to declare varibles with reserved words 
+
+var Harry = "Harry"
+var harry = "harry"
+var haRRy = "haRRy"
+// that's why JavaScript is case Sensitive 
+// but don't do this because your code is should readable , this is your first priority
