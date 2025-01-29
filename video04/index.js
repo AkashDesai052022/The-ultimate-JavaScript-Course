f3b51dc502b3649473a87f82ce2e6bee4a6eb998
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-29 10:39:31 +0530

    primitive and objects

---
+++ video04/index.js
@@ -0,0 +1,21 @@
+// nn ss bb u
+ 
+let a = null;
+let b = 345;
+let c = "akash";
+let d = Symbol("hey I am programmer");
+let e = BigInt("456");
+let f = true;
+let g = undefined;
+console.log(a, b, c, d, e, f, g);
+
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g);
+
+// objects in js
+const items = {
+    'harry': true,
+    'age': 30,
+    'qualification': 'IIT Kharagpur'
+}
+console.log(items['qualification']);
+// console.log(items['asdsd']);  // result : undefined
