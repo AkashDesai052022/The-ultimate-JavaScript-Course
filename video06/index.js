b32ce0949264a558051a9f40aaf5ffeb755a491b
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-29 12:21:14 +0530

    JS operators and expression

---
+++ video06/index.js
@@ -0,0 +1,49 @@
+// 77;
+// "harrry"
+// false;
+
+// these are expressions in js
+
+console.log("Operators in JS");
+
+// arithmetic operators
+// let a = 45;
+// let b = 4;
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a ** b = ", a**b);
+// console.log("a %  b = ", a%b);
+
+// Assignment operator
+
+// let assign = 5;
+
+// console.log("assign += ", assign += 4);
+// console.log("assign -= ", assign -= 4);
+// console.log("assign *= ", assign *= 4);
+// console.log("assign /= ", assign /= 4);
+// console.log("assign %= ", assign %= 4);
+// console.log("assign **= ", assign **= 2);
+
+// comparison operators
+
+// let x = 5;
+// let y = "4";
+// console.log("x == y is ", x == y);
+// console.log("x != y is ", x != y);
+// console.log("x === y is ", x === y);
+// console.log("x !== y is ", x !== y);
+// console.log("x > y is ", x > y);
+// console.log("x < y is ", x < y);
+// console.log("x >= y is ", x >= y);
+// console.log("x <= y is ", x <= y);
+
+// logical operators
+let p = 5;
+let q = 6;
+console.log(p < q && p == 5); // both should be true 
+console.log(p > q || p == 5); // either one should be true
+console.log(!false);   // not of true or false
+
