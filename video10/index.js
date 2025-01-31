020f89ee9e62f9946642fb35fdc9171c2dc7f93f
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-31 11:36:24 +0530

    while and do while loop

---
+++ video10/index.js
@@ -0,0 +1,15 @@
+// let n = prompt('eneter a value of n')
+let n = 50; 
+
+let i = 1;
+while(i <= n){
+    // console.log("I love you.");
+    i++;
+}
+
+let p = 0;
+let j = 8;
+do{
+    console.log(p);
+    p++
+}while(p < j)
