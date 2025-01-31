f64a0e645d8f357ba25c145c118f9a2a6ac37a64
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-31 13:41:01 +0530

    String methods and properties in JS

---
+++ video14/index.js
@@ -0,0 +1,34 @@
+// let str = 'harry\'s bhai' 
+// console.log(str); 
+// console.log(str.length); // here backslash is not in count
+
+
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+
+// console.log(str.slice(2, 4));
+// console.log(str.slice(3));
+
+
+// console.log(str.replace("bhai", "bhau"))
+
+// let friend = "anup"
+// console.log(str.concat( 'is friend of ' , friend, " and", ' akash'));
+
+// let str1 = '      meena'
+// console.log(str1);
+// console.log(str1.trim());
+
+// // strings are immutable . in order to access the character at an index we use following syntax.
+
+// let fr = "shubham"
+// console.log(name[0]);
+// console.log(fr[4]);
+
+
+let name = 'harry'
+for (let i  = 0; i  < name.length; i ++) {
+    const element = name[i];
+    console.log(element);
+}
+
