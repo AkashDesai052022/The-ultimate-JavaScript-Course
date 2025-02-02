34d74f3b6df988ff5ef54afc1d9d00aa59b814e9
AkashDesai052022 <akashdesai052022@gmail.com>
2025-02-02 20:42:12 +0530

    DOM , BOM and Window object in JS

---
+++ video28/index.js
@@ -0,0 +1,10 @@
+// window , BOM AND DOM
+
+console.log(window);
+console.log(document);
+console.log(document.body);
+document.body.style.backgroundColor = 'yellow'
+
+
+// Dom = document object model
+// bom = browser object model  = provides additional objcet by browser (host environment) alert prompt confirm is part of bom
