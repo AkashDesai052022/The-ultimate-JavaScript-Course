efc807a03b177bd1e30b89e57e56e3b528ad18b4
AkashDesai052022 <akashdesai052022@gmail.com>
2025-02-02 20:41:32 +0530

    alert, prompt and confirm in JS

---
+++ video27/index.js
@@ -0,0 +1,6 @@
+alrert('hello world')
+let p = prompt("enter a number", '24')  // last is default value;
+
+confirm("would you like to proceed?")  // cancel = fasle ; ok = true;
+
+// use madals instead of alert prompt, confirm
