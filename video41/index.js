985372c48ffc460b649af476cd4ee5f59f300f01
AkashDesai052022 <akashdesai052022@gmail.com>
2025-02-04 02:01:38 +0530

    innerHTML , outerHTML and properties in JS

---
+++ video41/index.js
@@ -0,0 +1,11 @@
+// chapter 8 : Events and other DOM Properties
+
+console.log(document.getElementsByTagName('span')[0])
+console.dir(document.getElementsByTagName('span')[0])
+
+// innerHTML
+// outerHTML
+
+//textContent
+
+// hidden property
