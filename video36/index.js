0d6dc00ce7db9340c44acfc96dbe22314cb8fc5c
AkashDesai052022 <akashdesai052022@gmail.com>
2025-02-04 00:54:19 +0530

    Searching the DOM in JS

---
+++ video36/index.js
@@ -0,0 +1,8 @@
+document.getElementById('firstcard').style.color = "red"
+
+document.getElementsByClassName('card-title')[0].style.color = 'blue'
+
+let c = document.querySelectorAll(".card-title")
+c[0].style.color = "green"
+c[1].style.color = "yellow"
+c[2].style.color = "orange"
